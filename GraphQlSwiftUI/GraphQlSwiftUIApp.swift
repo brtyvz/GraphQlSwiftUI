bed625d6f88b6b730873ba8fabd175fba772673f
Berat Yavuz <beratttyavuzz@gmail.com>
2022-06-14 22:28:39 +0300

    Initial Commit

---
+++ GraphQlSwiftUI/GraphQlSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphQlSwiftUIApp.swift
+//  GraphQlSwiftUI
+//
+//  Created by Berat Yavuz on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct GraphQlSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
